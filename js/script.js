5af075e0aac0b8b65827759f1745d19e357486a3
Gabriel Kaam <gabriel@kaam.fr>
2015-12-16 17:36:02 +0100

    Fixes textarea form

--- js/script.js
+++ js/script.js
@@ -4,12 +4,12 @@
   $( document ).ready(function onDocumentReady() {
     SameHeightAs();
 
-    $('.field[id]').on('focus', function() {
-      $('label[for=' + this.id + ']').parent().addClass('active')
+    $('.field[id], textarea[id]').on('focus', function() {
+      $('label[for=' + this.id + ']').parents('.input-field').addClass('active')
     });
-    $('.field[id]').on('blur', function() {
+    $('.field[id], textarea[id]').on('blur', function() {
       if('' === this.value)
-        $('label[for=' + this.id + ']').parent().removeClass('active')
+        $('label[for=' + this.id + ']').parents('.input-field').removeClass('active')
     });
     $('#equipe-voir-plus').on('click', function(event) {
       event.preventDefault();
