89d18ec2ca12727ce505ed9afc4a108e4fcd4af3
Gabriel Kaam <gabriel@kaam.fr>
2015-12-17 11:23:41 +0100

    Fixes form submission via JS

--- js/script.js
+++ js/script.js
@@ -1,19 +1,36 @@
 ;
 
+// from http://stackoverflow.com/questions/1184624/convert-form-data-to-javascript-object-with-jquery
+$.fn.serializeObject = function() {
+    var o = {};
+    var a = this.serializeArray();
+    $.each(a, function() {
+        if (o[this.name] !== undefined) {
+            if (!o[this.name].push) {
+                o[this.name] = [o[this.name]];
+            }
+            o[this.name].push(this.value || '');
+        } else {
+            o[this.name] = this.value || '';
+        }
+    });
+    return o;
+};
+
 (function() {
   $( document ).ready(function onDocumentReady() {
     SameHeightAs();
 
-    $('.field[id], textarea[id]').on('focus', function() {
+    $('.field[id], textarea[id]').on('focus change', function() {
       $('label[for=' + this.id + ']').parents('.input-field').addClass('active')
     });
-    $('.field[id], textarea[id]').on('blur', function() {
+    $('.field[id], textarea[id]').on('blur change', function() {
       if('' === this.value)
         $('label[for=' + this.id + ']').parents('.input-field').removeClass('active')
     });
     $('#equipe-voir-plus').on('click', function(event) {
       event.preventDefault();
-      $('#equipe-plus').slideToggle();
+      $('#equipe-plus').slideToggle()
     });
 
     $('#contact_form').on('submit', function(event) {
@@ -23,7 +40,8 @@
       $.ajax({
         url: $form.attr('action'),
         method: $form.attr('method'),
-        data: $form.serialize(),
+        data: { message: $form.serializeObject() },
+        dataType: 'json',
       })
       .done(function(data) {
         alert('Message bien envoyé ! Merci')
