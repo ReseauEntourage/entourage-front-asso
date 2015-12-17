c9c818cd2fc1d279f646a8ef2718f4288abb1652
Gabriel Kaam <gabriel@kaam.fr>
2015-12-17 11:48:29 +0100

    Now handles multiple envs

--- index.php
+++ index.php
@@ -1,1 +1,23 @@
-<?php include('home.html');
+<?php
+
+$_CONF = [
+  'development' => [
+    'API_BASE_URL' => 'https://entourage-back-preprod.herokuapp.com/api',
+    ],
+  'staging' => [
+    'API_BASE_URL' => 'https://entourage-back-preprod.herokuapp.com/api',
+    ],
+  'production' => [
+    'API_BASE_URL' => 'https://entourage-back.herokuapp.com/api',
+    ],
+];
+
+$DEFAULT_ENV = 'development';
+$ENV = getenv('ENV');
+
+if(!isset($_CONF[$ENV])) {
+  $ENV = $DEFAULT_ENV;
+}
+$CONF = $_CONF[$ENV];
+
+include 'home.html';
