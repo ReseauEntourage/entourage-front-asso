0a9a5c0b7897d6fc5e609bcbec8f3a8af3c3af67
Vincent Daubry <vdaubry@gmail.com>
2016-01-06 17:52:19 +0100

    add different url for staging and production environment

--- app.rb
+++ app.rb
@@ -1,8 +1,10 @@
 get '/' do
+  @messages_form_url = "#{ENV['BASE_URL']}/messages"
   erb :home
 end
 
 get '/adherer' do
+  @registration_requests_form_url = "#{ENV['BASE_URL']}/registration_requests"
   @s3_direct_post = S3_BUCKET.presigned_post(key: "logo_#{SecureRandom.uuid}_${filename}", success_action_status: '201', acl: 'public-read')
   erb :adherer
 end
---
+++ initializers/environment.rb
@@ -0,0 +1,5 @@
+ENV['BASE_URL']= if ENV['RACK_ENV']=='production'
+  "https://entourage-back.herokuapp.com/api/v0"
+else 
+  "https://entourage-back-preprod.herokuapp.com/api/v0"
+end
