c24335c360febbb4328082e8034ac9e0b63772a1
Grégoire Clermont <gregoire@entourage.social>
2017-10-19 15:25:30 +0200

    fix: minor improvements of the registration experience

--- app.rb
+++ app.rb
@@ -3,7 +3,7 @@ configure do
 end
 
 get '/' do
-  resp = Net::HTTP.get_response(URI.parse("#{ENV['BASE_URL']}/stats")).body
+  resp = Net::HTTP.get_response(URI.parse("#{ENV['BASE_URL']}/v0/stats")).body
   @stats = JSON.parse(resp)
   @messages_form_url = "#{ENV['BASE_URL']}/messages"
   response.headers['X-FRAME-OPTIONS']='ALLOWALL'
@@ -11,7 +11,7 @@ get '/' do
 end
 
 get '/adherer' do
-  @registration_requests_form_url = "#{ENV['BASE_URL']}/registration_requests"
+  @registration_requests_form_url = "#{ENV['BASE_URL']}/v1/registration_requests"
   @s3_direct_post = S3_BUCKET.presigned_post(key: "logo_#{SecureRandom.uuid}_${filename}", success_action_status: '201', acl: 'private')
   erb :adherer
 end
--- initializers/environment.rb
+++ initializers/environment.rb
@@ -1,5 +1,5 @@
-ENV['BASE_URL'] = if ENV['RACK_ENV']=='production'
-  "https://api.entourage.social/api/v0"
-else 
-  "https://entourage-back-preprod.herokuapp.com/api/v0"
+ENV['BASE_URL'] ||= if ENV['RACK_ENV']=='production'
+  "https://api.entourage.social/api"
+else
+  "https://entourage-back-preprod.herokuapp.com/api"
 end
