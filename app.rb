9cac539d0dc2346fb7d5c873d5456ac6f08f87f5
François Pellissier <francois.pellissier@gmail.com>
2018-01-10 15:57:29 +0100

    feature(redirect): redirect all site to 'https://www.entourage.social/associations/'

--- app.rb
+++ app.rb
@@ -5,19 +5,22 @@ configure do
 end
 
 get '/' do
-  resp = Net::HTTP.get_response(URI.parse("#{ENV['BASE_URL']}/v0/stats")).body
-  @stats = JSON.parse(resp)
-  @messages_form_url = "#{ENV['BASE_URL']}/messages"
-  response.headers['X-FRAME-OPTIONS']='ALLOWALL'
-  erb :home
+  redirect 'https://www.entourage.social/associations/'
+  # resp = Net::HTTP.get_response(URI.parse("#{ENV['BASE_URL']}/v0/stats")).body
+  # @stats = JSON.parse(resp)
+  # @messages_form_url = "#{ENV['BASE_URL']}/messages"
+  # response.headers['X-FRAME-OPTIONS']='ALLOWALL'
+  # erb :home
 end
 
 get '/adherer' do
-  @registration_requests_form_url = "#{ENV['BASE_URL']}/v1/registration_requests"
-  @s3_direct_post = S3_BUCKET.presigned_post(key: "logo_#{SecureRandom.uuid}_${filename}", success_action_status: '201', acl: 'private')
-  erb :adherer
+  redirect 'https://www.entourage.social/associations/'
+  # @registration_requests_form_url = "#{ENV['BASE_URL']}/v1/registration_requests"
+  # @s3_direct_post = S3_BUCKET.presigned_post(key: "logo_#{SecureRandom.uuid}_${filename}", success_action_status: '201', acl: 'private')
+  # erb :adherer
 end
 
 get '/sponsors' do
-  erb :sponsors
+  redirect 'https://www.entourage.social/associations/'
+  # erb :sponsors
 end
